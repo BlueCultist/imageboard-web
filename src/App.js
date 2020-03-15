605db2ad6baa5d0c0299c166df698c41df348c8a
Roch D'Amour <roch.damour@gmail.com>
2020-03-15 00:45:41 -0400

    Played with useState, fixed axios API URL

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
-import React from "react";
-import axios from "./utils/axios";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import WeatherDisplay from "./Components/weatherDisplay";
+
+let num = 1;
 
 function App() {
-  async function getFacts() {
-    try {
-      const response = await axios.get(`/api/breeds/image/random`);
-      console.log(response);
-    } catch (e) {
-      console.error(e);
-    }
-  }
+  const [word, setWord] = useState("word");
+
+  const clickHandler = text => () => {
+    num++;
+    setWord(text + " " + num);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
-        <p onClick={getFacts}>Click to get stuff</p>
+        <p onClick={clickHandler("bouette")}>{word}</p>
       </header>
     </div>
   );
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dog.ceo",
+  baseURL: "http://localhost:5001",
   timeout: 3000,
   headers: {
     "Content-Type": "application/json"
