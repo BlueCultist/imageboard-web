b7d43c53611e83b4ff1d56b8b22cb7926d8ea535
Roch D'Amour <roch.damour@gmail.com>
2020-03-14 23:57:14 -0400

    Add axios wrapper & first HTTP get function

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "./utils/axios";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  async function getFacts() {
+    try {
+      const response = await axios.get(`/api/breeds/image/random`);
+      console.log(response);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +28,8 @@ function App() {
         >
           Learn React
         </a>
+
+        <p onClick={getFacts}>Click to get stuff</p>
       </header>
     </div>
   );
